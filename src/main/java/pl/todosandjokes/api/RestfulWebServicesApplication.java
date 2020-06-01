441f94ae467617c3e871f05729a522108e6b33c6
Mategrze <mgrzelak1997@wp.pl>
2020-06-01 23:49:22 +0200

    Change package

--- src/main/java/pl/todosandjokes/api/RestfulWebServicesApplication.java
+++ src/main/java/pl/todosandjokes/api/RestfulWebServicesApplication.java
@@ -1,4 +1,4 @@
-package com.rest.webservices.restfulwebservices;
+package pl.todosandjokes.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
