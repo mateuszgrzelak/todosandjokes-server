7a1da147c5002c33a5da16e6fa41ba32a3c2a4a4
Mategrze <mgrzelak1997@wp.pl>
2020-06-09 22:46:13 +0200

    Create JWT authorization filter class

---
+++ src/main/java/pl/todosandjokes/api/configuration/JWTAuthorizationFilter.java
@@ -0,0 +1,65 @@
+package pl.todosandjokes.api.configuration;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static pl.todosandjokes.api.configuration.SecurityConstants.*;
+
+
+public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
+
+    public JWTAuthorizationFilter(AuthenticationManager authManager) {
+        super(authManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest req,
+                                    HttpServletResponse res,
+                                    FilterChain chain) throws IOException, ServletException {
+        String header = req.getHeader(HEADER_STRING);
+
+        if (header == null || !header.startsWith(TOKEN_PREFIX)) {
+            chain.doFilter(req, res);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(req);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(req, res);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
+        String jwt = request.getHeader(HEADER_STRING);
+        if (jwt != null) {
+            jwt = jwt.substring(7);
+            // parse the token.
+            Claims claims;
+            String username;
+            try {
+                claims = (Claims)Jwts.parserBuilder()
+                        .setSigningKey(SECRET_KEY)
+                        .build()
+                        .parse(jwt).getBody();
+                username = claims.getSubject();
+                return new UsernamePasswordAuthenticationToken(username, null, new ArrayList<>());
+            }catch(Exception e){
+                e.printStackTrace();
+                return null;
+            }
+        }
+        return null;
+    }
+
+}
